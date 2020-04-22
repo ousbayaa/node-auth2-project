439f3ae2713b236a04d1767dc560058c5cc2159d
Ous Bayaa <bayaaous@gmail.com>
2020-04-21 21:49:32 -0700

    mvp

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 const authRouter = require('../auth/auth-router');
-const userRouter = require('../users/user-router');
+const usersRouter = require('../users/users-router');
 const authenticator = require('../auth/authenticator');
 
 const server = express();
@@ -13,7 +13,7 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/api/auth', authRouter);
-server.use('/api/users', authenticator, userRouter);
+server.use('/api/users', authenticator, usersRouter);
 
 server.get('/', (req, res) => {
     res.send('Up and Running!!');
